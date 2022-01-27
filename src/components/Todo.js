6290f0095d6475230194d4eaa84d61f11815b6ee
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-01-27 10:32:41 +0530

    Initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,61 @@
+import "./Todo.css";
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {addToDo, deleteToDo, removeToDo} from "../actions";
+import "../reducers/todoReducers";
+
+const Todo = () => {
+
+    const [inputData, setInputData] = useState("");
+    const dispatch = useDispatch();
+    const list = useSelector((state) => state.todoReducers.list)
+
+    return (
+        <>
+            <div className="main-div">
+                <div className="child-div">
+                    <figure>
+                        <figcaption>Add the list here</figcaption>
+                    </figure>
+                    <div className="add-Items">
+                        <input type="text"
+                               placeholder="Add item"
+                               value={inputData}
+                               onChange={(e) => {
+                                   setInputData(e.target.value)
+                               }}
+                        />
+                        <i className="fa fa-plus add-btn"
+                           title="Add item"
+                        onClick={() => {
+                            dispatch(addToDo(inputData))
+                            setInputData("")
+                        }}/>
+                    </div>
+                    <div className="showItems">
+                        {
+                            list.map((item) => {
+                                return (
+                                    <div className="eachItem"
+                                        key={item.id}
+                                    >
+                                        <h3>{item.data}</h3>
+                                        <div className="todo-btn">
+                                            <i className="far fa-trash-alt add-btn"
+                                               title="Delete Item"
+                                               onClick={() => {
+                                                   dispatch(deleteToDo(item.id))
+                                               }}/>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Todo;
