47c7f5668b216485ee3afa78403d8cf8f281b350
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-01-28 10:41:15 +0530

    Added a randomNumber generator as the id

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,20 @@
 export const addToDo = (data) => {
+
+    const generateRandomNumber = new Date().getTime().toString()+(Math.floor(Math.random()*100000));
+
     return {
         type: "ADD_TODO",
         payload: {
+            id: generateRandomNumber,
             data: data
         }
     }
-}
 
+}
 export const deleteToDo = (id) => {
     return {
         type: "DELETE_TODO",
         id: id,
     }
-}
 
-export const removeToDo = () => {
-    return {
-        type: "REMOVE_TODO"
-    }
 }
